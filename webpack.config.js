9f1e29ed57d383b7deba3ae4a4aa0e34faad73e2
Aleksei Golubkov <nuriver@gmail.com>
2024-04-30 21:02:15 +0200

    feat: add index.ts and index.html to the project

--- webpack.config.js
+++ webpack.config.js
@@ -7,64 +7,64 @@ const CopyPlugin = require('copy-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const baseConfig = {
-  entry: path.resolve(__dirname, './src/index.ts'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: false,
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                    'css-loader',
+                ],
             },
-          },
-          'css-loader',
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
+                type: 'asset/resource',
+            },
         ],
-      },
-      { test: /\.ts$/i, use: 'ts-loader' },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
-        type: 'asset/resource',
-      },
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist/assets'),
+                },
+                {
+                    from: './src/routing',
+                },
+            ],
+        }),
+        new FaviconsWebpackPlugin({
+            logo: './src/assets/chat-favicon.png',
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, './dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/assets'),
-          to: path.resolve(__dirname, 'dist/assets'),
-        },
-        {
-          from: './src/routing',
-        },
-      ],
-    }),
-    new FaviconsWebpackPlugin({
-      logo: './src/assets/chat-favicon.png',
-    }),
-  ],
 };
 
 module.exports = ({ mode }) => {
-  const isProductionMode = mode === 'prod';
-  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-  return merge(baseConfig, envConfig);
+    return merge(baseConfig, envConfig);
 };
