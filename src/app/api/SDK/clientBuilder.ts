2dc4213ccd8003ce82d71841c5652993fa1a4bfc
Aleksei Golubkov <nuriver@gmail.com>
2024-05-05 22:06:03 +0200

    feat: add clientBuilder.ts

---
+++ src/app/api/SDK/clientBuilder.ts
@@ -0,0 +1,28 @@
+import { ClientBuilder, type AuthMiddlewareOptions, type HttpMiddlewareOptions } from '@commercetools/sdk-client-v2';
+
+const projectKey = process.env.CTP_PROJECT_KEY as string;
+const scopes = [process.env.CTP_SCOPES as string];
+const clientId = process.env.CTP_CLIENT_ID as string;
+const clientSecret = process.env.CTP_CLIENT_SECRET as string;
+
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: 'https://auth.europe-west1.gcp.commercetools.com',
+  projectKey,
+  credentials: {
+    clientId,
+    clientSecret,
+  },
+  scopes,
+};
+
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: 'https://api.europe-west1.gcp.commercetools.com',
+};
+
+const ctpClient = new ClientBuilder()
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware() // Include middleware for logging
+  .build();
+
+export default ctpClient;
