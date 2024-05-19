38ddb5d65b8ac3204fd59df3f5fc37a01738d532
aauroraaborealis <ludanechai@mail.ru>
2024-05-19 22:12:41 +0300

    feat: add 404 page

--- src/app/utilities/createElement.ts
+++ src/app/utilities/createElement.ts
@@ -3,13 +3,13 @@ import ElementMap from '../types/types';
 function createElement<K extends keyof ElementMap>(
   tag: K,
   className: string[],
-  parent: HTMLElement,
+  parent?: HTMLElement,
   innerHtml?: string
 ): ElementMap[K] {
   const element = document.createElement(tag);
   className.forEach((name) => element.classList.add(name));
   if (innerHtml !== undefined) element.innerHTML = innerHtml;
-  parent.append(element);
+  if (parent) parent.append(element);
   return element;
 }
 
