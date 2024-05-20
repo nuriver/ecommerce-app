163684a6e3a93ede9576cb73f9ad61ee7e06ac9a
Aleksei Golubkov <nuriver@gmail.com>
2024-05-20 14:53:43 +0200

    refactor: update footer

--- src/app/components/footer/index.ts
+++ src/app/components/footer/index.ts
@@ -4,7 +4,7 @@ export default function createFooter(parent: HTMLElement): HTMLElement {
   const footerBlock = createElement('div', ['footer'], parent);
   const footerCont = createElement('div', ['footer-cont'], footerBlock);
   createElement('p', ['footer-text'], footerCont, 'Rolling Scopes School');
-  createElement('p', ['footer-text'], footerCont, 'MADE WITH LOVE');
+  createElement('p', ['footer-text'], footerCont, 'FUNC CRAFTERS');
   createElement('p', ['footer-text'], footerCont, '2024');
   return footerBlock;
 }
