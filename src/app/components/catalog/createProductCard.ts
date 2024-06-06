a4d45cf01cf6527787fdfeacc9d35e96d520c806
Aleksei Golubkov <nuriver@gmail.com>
2024-06-06 22:44:23 +0200

    feat: add cart button to product card

--- src/app/components/catalog/createProductCard.ts
+++ src/app/components/catalog/createProductCard.ts
@@ -8,6 +8,10 @@ export default function createProductCard(product: ProductData, parent: HTMLElem
   productImg.style.backgroundImage = `url(${product.image})`;
   createElement('p', ['product-name'], productCard, product.name);
   createElement('p', ['product-description'], productCard, product.description);
+  const addToCartButtonInnerHtml = '<span class="cart-icon"></span>';
+  const addToCartButton = createElement('button', ['add-to-cart-button'], productCard, addToCartButtonInnerHtml);
+  addToCartButton.id = product.id;
+
   if (product.discountPrice) {
     createElement('p', ['product-price', 'product-price-discount'], productCard, `${product.discountPrice}$`);
     createElement('p', ['product-price', 'product-price-old'], productCard, `${product.price}$`);
